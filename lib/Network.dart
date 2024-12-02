d14fa011a1337fd78afe769dfe1b4ea38244212e
dilrun2001 <dilrun2001@gmail.com>
2024-12-03 08:23:21 +0900

    유저생성 버그고침

--- lib/Network.dart
+++ lib/Network.dart
@@ -37,7 +37,6 @@ class Network {
 
   Future<dynamic> createuser(String user, String password, String level) async {//post로 3개의 string을 보내고 받지는 않는다
     if(level.isEmpty) {level = '구매자';}
-    print(level);
     final uri = Uri.parse(url); // 서버의 엔드포인트 URL
     final headers = {'Content-Type': 'application/json'};
     final body = jsonEncode({
