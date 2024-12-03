95811ebb4af37a8c7fb45b52a0ef5ac8865cd3c3
kjune922 <kjune922@naver.com>
2024-12-03 10:48:14 +0900

    select.dart추가, 숙련자창추가

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,3 +3,7 @@ String? registeredUsername;
 String? registeredPassword;
 String? registeredUserLevel;
 String? productName;
+
+// 추가된 글로벌 변수
+List<Map<String, dynamic>> expertCartComponents = []; // 숙련자용 장바구니 데이터
+bool expertCartInitialized = false; // 장바구니 데이터 초기화 여부
